80e1361f5d48532413846181570ad0182d855370
gelafin <themarkymarkers@gmail.com>
2022-02-22 18:42:14 -0800

    enable hybrid p2p communication with fixed messages

--- http_client.py
+++ http_client.py
@@ -1,51 +1,55 @@
 # Author: Mark Mendez
-# Date: 01/09/2022
+# Date: 02/22/2022
 # used starter code and concepts from "Computer Networking: A Top-Down Approach" by James F. Kurose and Keith Ross
 
 from socket import *
 
+PORT_NUMBER = 8011
 
-# configure server details necessary for connection
-serverName = 'gaia.cs.umass.edu'  # server hostname or IP address
-serverPort = 80  # standard Web port
+# Configure server details necessary for connection.
+serverName = 'localhost'  # server hostname or IP address
+serverPort = PORT_NUMBER  # arbitrary port > 1023 (source: previous assignment instructions) matching server
 
-# choose a message to send to the server specified above
-message = 'GET /wireshark-labs/HTTP-wireshark-file3.html HTTP/1.1\r\nHost:gaia.cs.umass.edu\r\n\r\n'
+# Choose a message to send to the server specified above
+message = 'GET /imaginary-page.html HTTP/1.1\r\nHost:example.example\r\n\r\n'
 
-# create a client socket,
+# Create a client socket,
 # using default address family (SOCK_STREAM means to use TCP)
 client_socket = socket(family=AF_INET, type=SOCK_STREAM)
 
-# connect to the server specified above
+# Connect to the server specified above
 client_socket.connect((serverName, serverPort))
 
-# send message to server
-print('Request: ' + message)
+# Send message to server
+print(f'[client]\nRequest:\n{message}')
 client_socket.send(message.encode())
 
-# receive first piece of response from server
+# Receive first piece of response from server.
 # Note from recv() docs:
 # For best match with hardware and network realities,
 # the value of bufsize should be a relatively small power of 2, for example, 4096
-raw_res = client_socket.recv(1024)
+BUFFER_SIZE = 1024
+raw_res = client_socket.recv(BUFFER_SIZE)
 
 # Receive remaining pieces of response from server in a loop,
 # to enable arbitrarily large files to be received.
-# The gaia.cs.umass.edu server will close the connection after sending its data,
+# The server in this project will close the connection after sending its data,
 # so detect when recv() returns <= 0 bytes. (source: assignment document)
+# TODO: this loop condition could be updated to respect a protocol indicating message length
 res_message = ''
 while len(raw_res) > 0:
-    # remember to decode raw byte response message,
+    # Decode raw byte response message,
     # then append this segment of the message
     res_message += raw_res.decode()
 
-    # receive next segment of the message or a bytes object of length 0
-    raw_res = client_socket.recv(1024)
+    # Receive next piece of the message or a bytes object of length 0
+    raw_res = client_socket.recv(BUFFER_SIZE)
 
-# print server response
+# Print server response
 if res_message != '':
-    print('[RECV] - length:', len(res_message))
+    print('[client]\n*received message*')
+    print(f'\tlength: {len(res_message)}')
     print(res_message)
 
-# close socket connection
+# Close socket connection
 client_socket.close()
--- http_server.py
+++ http_server.py
@@ -1,44 +1,49 @@
 # Author: Mark Mendez
-# Date: 01/09/2022
+# Date: 02/22/2022
 # used starter code and concepts from "Computer Networking: A Top-Down Approach" by James F. Kurose and Keith Ross
 
 from socket import *
 
-# configure server details necessary for connection
-# this simple example only works with localhost
-serverName = 'localhost'  # server hostname or IP address
-serverPort = 8011  # arbitrary port > 1023 (source: previous assignment instructions)
+PORT_NUMBER = 8011
 
-# choose a message to send to clients who connect
+# Configure server details necessary for connection.
+# This simple example only works with localhost.
+serverName = 'localhost'  # server hostname or IP address
+serverPort = PORT_NUMBER  # arbitrary port > 1023 (source: previous assignment instructions) matching client
+
+# Choose a message to send to clients who connect
 res_message = "HTTP/1.1 200 OK\r\n" \
               "Content-Type: text/html; charset=UTF-8\r\n\r\n" \
-              "<html>Congratulations! You've downloaded the first Wireshark lab file!</html>\r\n"
+              "<html>Congratulations! You found me!</html>\r\n"
 
-# create a server socket,
-# using default address family (SOCK_STREAM means to use TCP)
+# Create a server socket,
+# Using default address family (SOCK_STREAM means to use TCP)
 server_socket = socket(family=AF_INET, type=SOCK_STREAM)
 
-# listen for connections
+# Listen for connections
 server_socket.bind(('', serverPort))
 server_socket.listen(1)  # allow max of 1 queued connection
 
-# accept and handle requests
+# Accept and handle requests
 while True:
-    connection_socket, addr = server_socket.accept()
-    # Remember to decode raw byte response message,
+    # Accept a new connection
+    connection_socket, client_address = server_socket.accept()
+
+    # Decode raw byte response message,
     # then append this segment of the message.
     # Note from recv() docs:
     # For best match with hardware and network realities,
     # the value of bufsize should be a relatively small power of 2, for example, 4096
-    req_message_raw = connection_socket.recv(1024).decode()
+    BUFFER_SIZE = 1024
+    req_message_raw = connection_socket.recv(BUFFER_SIZE).decode()
 
-    # print this socket's new request
-    print('Connected by ', '(', serverName, ',', serverPort, ')')
-    print('\nReceived:', req_message_raw)
-    print('\nSending>>>>>>>', res_message, '\n<<<<<<<')
+    # Print this socket's new request
+    print(f'[server]\nConnected by {serverName}:{serverPort}')
+    print(f'\n[server]\nReceived: {req_message_raw}')
+    print(f'\n[server]\nSending response: {res_message}')
 
-    # send the response message
+    # Send the response message
     connection_socket.send(res_message.encode())
 
-    # close the connection
+    # Close the connection
     connection_socket.close()
