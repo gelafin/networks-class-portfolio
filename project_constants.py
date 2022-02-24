e59b08a91c25b7713878252e180d86d23aaff879
gelafin <themarkymarkers@gmail.com>
2022-02-23 16:09:45 -0800

    redesign to handle unlimited incoming packets

--- project_constants.py
+++ project_constants.py
@@ -1,4 +1,16 @@
 # Author: Mark Mendez
 # Date: 02/23/2022
-# Description: Deciphers encrypted messages from Enigma machine.
-#              Prints out results.
+
+"""Define all constants used by http socket programs"""
+QUIT_MESSAGE = '\\q'
+PORT_NUMBER = 8011
+# Note from recv() docs:
+# For best match with hardware and network realities,
+# the value of bufsize should be a relatively small power of 2, for example, 4096
+BUFFER_SIZE = 1024
+
+# Configure server details necessary for connection.
+# This simple example only works with localhost.
+SERVER_NAME = 'localhost'  # server hostname or IP address
+SERVER_PORT = PORT_NUMBER  # arbitrary port > 1023 (source: previous assignment instructions) matching client
+
